5cbfc81897f046b7582d2f63eccc7cca5474cd26
Evelin Garcia <e.garcia.santander@gmail.com>
2024-10-30 20:07:58 -0300

    ajuste gitignore

--- proyecto_tienda/static/js/scripts.js
+++ proyecto_tienda/static/js/scripts.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     var navlink = document.querySelectorAll('nav a');
 
+    //cambiar color de fondo de los links
     navlink.forEach(function(link){
         link.addEventListener("mouseover", function(){
             link.style.backgroundColor = "#ffe4c4";
