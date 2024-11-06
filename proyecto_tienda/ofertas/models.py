c21eb7dbdea53474d280b6ef9a7a7fad4f74f017
Evelin Garcia <e.garcia.santander@gmail.com>
2024-11-05 22:26:13 -0300

    adición de filtros a ofertas

--- proyecto_tienda/ofertas/models.py
+++ proyecto_tienda/ofertas/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from productos.models import Producto
 
 # Create your models here.
 
+class Oferta(models.Model):
+    producto=models.ForeignKey(Producto, on_delete=models.CASCADE)
+    porcentaje_descuento=models.DecimalField(max_digits=5, decimal_places=2)
+    fecha_inicio=models.DateTimeField()
+    fecha_fin=models.DateTimeField()
+    
+    def __str__(self):
+        return f"Oferta en {self.producto.nombre} - {self.porcentaje_descuento}% de descuento"
+    
+    @property
+    def precio_oferta(self):
+        return self.producto.precio*(1-(self.porcentaje_descuento/100))
+    
--- proyecto_tienda/ofertas/views.py
+++ proyecto_tienda/ofertas/views.py
@@ -1,5 +1,15 @@
+from datetime import datetime
 from django.shortcuts import render
+from .models import Oferta
 
 # Create your views here.
+current_date=datetime.now()
+ofertas=Oferta.objects.filter(fecha_inicio__lte=current_date, fecha_fin__gte=current_date)
+
 def index(request):
-    return render(request, 'ofertas/index.html')
+    context={
+#        'is_special_offer': False  #Se Cambia a True para si y False para no (para que aparezca si o no)        
+        'current_date':current_date,
+        'ofertas':ofertas
+    }
+    return render(request, 'ofertas/index.html', context)
